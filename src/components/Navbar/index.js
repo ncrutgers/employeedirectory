e1bc77dcee33c0b679d1ed007c816938063425fa
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-08-09 14:10:14 -0400

    Created navbar component & added stylesheet

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+        
+function Navbar() {
+    return(
+        
+            <nav className="navbar navbar-dark">
+                <div className="center">
+                    <a className="navbar-brand text" href="#">Employee Directory</a>
+                </div>
+            </nav>
+        
+    );    
+}
+
+export default Navbar;
